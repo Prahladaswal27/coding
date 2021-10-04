e45414317f6935161472acf72e822da225a210a0
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2021-10-04 20:09:49 +0530

    Update Practical 2_Task 1.cpp

--- OOPs with C++/Practical 2/Practical 2_Task 1.cpp
+++ OOPs with C++/Practical 2/Practical 2_Task 1.cpp
@@ -1,1 +1,33 @@
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int n,i,count=0;
+  cout<<"Enter the no.: ";
+  cin>>n;
+  
+  for(i=2:i<=n/2;i++)
+  {
+    if(n%i==0)
+    {
+      count =1;
+      break;
+    }
+  }
+  
+  if(n==1)
+    cout<<"\n1 is neither prime nor composite";
+  else
+  {
+    if(count==0)
+        cout<<n<<" is prime no.";
+    else
+         cout<<n<<" is not a prime no.";
+  }
+  
+  return 0;
+  
+}
+    
