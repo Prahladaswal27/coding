f5a7e5f04878be0ed8c35b3b6c14ee0f959653d8
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2021-10-05 23:47:04 +0530

    Update Practical 4_Task 2.cpp

--- OOPs with C++/Practical 4/Practical 4_Task 2.cpp
+++ OOPs with C++/Practical 4/Practical 4_Task 2.cpp
@@ -12,7 +12,36 @@ namespace first
    }
 }
 
+namespace second
+{
+   float add(float a,float b)
+   {
+      return (a+b);
+   }
+}
+
+int main()
+{
+   int sum;
+   float a;
+   
+   sum= first::add(3,3);
+   a= second::add(3.6,7.8);
+   
+   cout<<"\nSum is: "<<sum;
+   cout<<"\nADD is: "<<a;
+   
+   //cout<<"\naddition of two is: "<<add(3,4.7f);
+   
+   return 0;
+}
+
+
+
   
+   
+   
+   
 
     
 
@@ -20,48 +49,45 @@ namespace first
 
 
 
-namespace second
 
-{
 
-  float add(float a,float b)
 
-  {
-
-    return (a+b);
-
-  }
 
   
 
-}
+  
 
-int main()
-
-{
-
-  int sum;
-
-  float a;
-
-  sum= first::add(3,3);
-
-  a= second::add(3.6,7.8);
+    
 
   
 
-  cout<<"\nSum is: "<<sum;
+  
 
-  cout<<"\nADD is: "<<a;
+
+
+
+
+
 
   
 
-  //cout<<"\naddition of two is: "<<add(3,4.7f);
+  
 
   
 
-  return 0;
+  
 
   
 
-}
+  
+  
+
+  
+
+  
+
+  
+
+  
+
+
