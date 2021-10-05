de5e376e404ba069e7662407d53c720933670d65
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2021-10-05 23:32:25 +0530

    Update Practical 3_Task 4.cpp

--- OOPs with C++/Practical 3/Practical 3_Task 4.cpp
+++ OOPs with C++/Practical 3/Practical 3_Task 4.cpp
@@ -1,1 +1,18 @@
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  string name;
+  cout<<"Enter the name: ";
+  getline(cin,name);
+  
+  cout<<"\nName: "<<name;
+  
+  return 0;
+}
+
+
+
+  
