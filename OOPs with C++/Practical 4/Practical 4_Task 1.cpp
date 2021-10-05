8b5ec887063a3c3a85fff4ccd3f7b9974dd25342
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2021-10-05 23:36:32 +0530

    Update Practical 4_Task 1.cpp

--- OOPs with C++/Practical 4/Practical 4_Task 1.cpp
+++ OOPs with C++/Practical 4/Practical 4_Task 1.cpp
@@ -1,1 +1,37 @@
 
+#include<iostream>
+using namespace std;
+
+namespace first 
+{
+  int add(int a,int b)
+  {
+    return (a+b);
+  }
+}
+
+namespace second
+
+{
+
+  float add(float a,float b)
+  {
+    return (a+b);
+  }
+  
+}
+
+int main()
+{
+  int sum;
+  float a;
+  sum= fir::
+
+
+
+
+
+
+
+      
+
