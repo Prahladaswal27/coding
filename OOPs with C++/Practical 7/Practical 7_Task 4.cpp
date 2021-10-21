9b9ad1da64a6a9f49691793b017c4cdf1d99821f
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2021-10-21 23:03:50 +0530

    Update Practical 7_Task 4.cpp

--- OOPs with C++/Practical 7/Practical 7_Task 4.cpp
+++ OOPs with C++/Practical 7/Practical 7_Task 4.cpp
@@ -1,1 +1,40 @@
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a[3][3]={1,2,3,4,5,6,7,8,9};
+    int i,j;
+    
+    cout<<"\nArray A"<<endl;
+    
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+            cout<<a[i][j]<<" ";
+        
+        cout<<endl;
+    }
+    
+    cout<<"\nRestore array A"<<endl;
+    
+    for(i=0;i<3;i++)
+    {
+       for(j=0;j<3;j++)
+       {
+           a[i][j]=(10-a[i][j]);
+           
+           cout<<a[i][j]<<" ";
+           
+       }
+       
+       cout<<endl;
+       
+    }
+    
+    cout<<"\nElements a[2][0] after restore value: "<<a[2][0]<<endl;
+    
+    return 0;
+    
+}
