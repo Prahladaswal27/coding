aee6d907ea94ef12cffb8a1db95f3d30d5b211f1
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2021-10-21 22:59:33 +0530

    Update Practical 7_Task 2.cpp

--- OOPs with C++/Practical 7/Practical 7_Task 2.cpp
+++ OOPs with C++/Practical 7/Practical 7_Task 2.cpp
@@ -1,1 +1,42 @@
 
+#include<iostream>
+#include<ctime>
+using namespace std;
+
+int main()
+
+{
+     int ch;
+     time_t now=time(0);
+     
+     tm *dmy=localtime(&now);
+     cout<<"\nEnter your choice: ";
+     cin>>ch;
+     
+     switch(ch)
+     {
+        case 1: 
+           cout<<"\nCurrent Year: "<<1900 + dmy->tm_year<<endl;
+           break;
+        
+        case 2:
+            cout<<"\nCurrent Month: "<<1 + dmy->tm_mon<<endl;
+            break;
+            
+        case 3:
+             cout<<"\nCurrent Day: "<< dmy->tm_wday<<endl;
+             break;
+        
+        case 4:
+             cout<<"\nNot Applicable"<<endl;
+             break;
+             
+        default:
+             cout<<"\nWrong choice"<<endl;
+             break;
+             
+      }
+      
+      return 0;
+      
+}
