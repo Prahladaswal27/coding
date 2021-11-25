8705fcd1894430e2a85276b5d02d9a42e89d33dd
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2021-11-25 18:46:12 +0530

    Update Practical 8_Task 3_V2.cpp

--- OOPs with C++/Practical 8/Practical 8_Task 3_V2.cpp
+++ OOPs with C++/Practical 8/Practical 8_Task 3_V2.cpp
@@ -1,1 +1,25 @@
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int a=10;
+   int *p;
+   int **q;
+   
+   p=&a;
+   q=&p;
+   
+   cout<<*p<<endl;
+   cout<<**q<<endl;
+   
+   **q=**q/5;
+   
+   cout<<"Value of **q is: "<<**q<<endl;
+   cout<<"Value of a is: "<<a<<endl;
+   
+   return 0;
+   
+}
+
