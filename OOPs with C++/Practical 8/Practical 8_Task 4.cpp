752340001236f1c128339c4c09470467ed45a2a1
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2021-11-25 18:47:12 +0530

    Update Practical 8_Task 4.cpp

--- OOPs with C++/Practical 8/Practical 8_Task 4.cpp
+++ OOPs with C++/Practical 8/Practical 8_Task 4.cpp
@@ -1,1 +1,33 @@
 
+#include<iostream>
+using namespace std;
+
+int factorial(int);
+
+int main()
+{
+   int n,res;
+   
+   cout<<"\nEnter the number: ";
+   cin>>n;
+   
+   res=factorial(n);
+   
+   cout<<"\nFactorial of "<<n<<" is: "<<res<<endl;
+   
+   return 0;
+
+}
+
+int factorial(int num)
+{
+   int i,fact=1;
+   
+   for(i=1;i<=num;i++)
+   {
+       fact=fact*i;
+   }
+   
+   return fact;
+
+}
