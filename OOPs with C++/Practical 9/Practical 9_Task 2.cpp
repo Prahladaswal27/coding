49103090ae04dede1476439b3e3800569043a1e4
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2021-11-30 14:31:34 +0530

    Update Practical 9_Task 2.cpp

--- OOPs with C++/Practical 9/Practical 9_Task 2.cpp
+++ OOPs with C++/Practical 9/Practical 9_Task 2.cpp
@@ -1,1 +1,54 @@
 
+#include<iostream>
+using namespace std;
+
+class area
+{
+   private:
+       int len,wid,side;
+   public:
+     area(int l,int w)
+     {
+        len=l;
+        wid=w;
+        print_area(len,wid);
+     }
+     
+     area(int s)
+     {
+         side=s;
+         print_area(side);
+     }
+     
+     void print_area(int l,int w)
+     {
+         float ar;
+         ar=l*w;
+         cout<<"\nArea of rectangle is: "<<ar<<endl;
+     }
+     
+     void print_area(int s)
+     {
+         float ar2;
+         ar2=s*s;
+         cout<<"\nArea of square is: "<<ar2<<endl;
+     }
+     
+};
+
+int main()
+{
+   int l,w,s;
+   
+   cout<<"\nEnter the length and width of rectangle: ";
+   cin>>l>>w;
+   cout<<"\nEnter the side of the square: ";
+   cin>>s;
+   
+   area obj(l,w);
+   area obj2(s);
+   
+   return 0;
+   
+}
+
