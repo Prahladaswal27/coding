b6c03fa7b657d727fd81761b36c8f142714afd9f
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2021-11-30 14:34:49 +0530

    Update Practical 9_Task 6.cpp

--- OOPs with C++/Practical 9/Practical 9_Task 6.cpp
+++ OOPs with C++/Practical 9/Practical 9_Task 6.cpp
@@ -1,1 +1,21 @@
 
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+   cout<<"\nUSE OF endl"<<endl;
+   cout<<"GEHU"<<endl<<"DEHRADUN"<<endl<<"UTTARAKHAND"<<endl;
+   
+   cout<<"\nUSE OF setw()\n";
+   cout<<setw(8)<<"DEHRADUN"<<endl;
+   
+   cout<<"\nUSE OF setfill()\n";
+   cout<<setw(12)<<setfill('*')<<"DEHRADUN"<<endl;
+   
+   return 0;
+   
+}
+
