c722bc71271839a85d0ed8fa26fb7884da54a24a
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2021-11-30 14:37:34 +0530

    Update Practical 10_Task 3.cpp

--- OOPs with C++/Practical 10/Practical 10_Task 3.cpp
+++ OOPs with C++/Practical 10/Practical 10_Task 3.cpp
@@ -1,1 +1,35 @@
 
+#include<iostream>
+using namespace std;
+
+class remainder
+{
+   private:
+      int a,b;
+      
+   public:
+      remainder(int a,int b)
+      {
+         this->a=a;
+         this->b=b; 
+      }
+      
+      int res()
+      {
+         return(a%b);
+      }
+      
+};
+
+int main()
+{
+    int a,b;
+    cout<<"\nEnter the two no.: ";
+    cin>>a>>b;
+    
+    remainder r(a,b);
+    cout<<"\nRemainder of two no. is: "<<r.res()<<endl;
+    
+    return 0;
+    
+}
