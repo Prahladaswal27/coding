5f1db640348f2bd24d0d5eb01e9d46243015b604
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2021-11-30 14:32:40 +0530

    Update Practical 9_Task 3.cpp

--- OOPs with C++/Practical 9/Practical 9_Task 3.cpp
+++ OOPs with C++/Practical 9/Practical 9_Task 3.cpp
@@ -1,1 +1,38 @@
 
+#include<iostream>
+using namespace std;
+
+
+class test
+{
+    private: 
+    static int count;
+    
+    public:
+    
+       test()
+       {  
+           count++;
+       }
+       
+       static int obj_count()
+       {
+          return count;
+       }
+
+};
+
+int test::count;
+
+              
+int main()
+{
+    test t1,t2,t3,t4;
+    int c;
+    c=test::obj_count();
+    
+    cout<<"\nNumber of objects are: "<<c<<endl;
+    
+    return 0;
+    
+}
