9c9d1043ae59cddf348dbaa0918eb384e08a960c
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2021-11-30 14:39:16 +0530

    Update Practical 10_Task 4_B.cpp

--- OOPs with C++/Practical 10/Practical 10_Task 4_B.cpp
+++ OOPs with C++/Practical 10/Practical 10_Task 4_B.cpp
@@ -1,1 +1,45 @@
 
+#include<iostream>
+using namespace std;
+
+class overload
+{
+   int a;
+   public:
+      void get(int n)
+      {
+          this->a=n;
+      }
+      
+      void display()
+      {
+         cout<<"\nSum is: "<<a<<endl;
+      }
+      
+      overload operator +(overload &var)
+      {
+         overload obj;
+         obj.a=this->a+var.a;
+         return obj;
+      }
+      
+};
+
+int main()
+{
+    int n1,n2;
+    cout<<"\nEnter the two numbers: ";
+    cin>>n1>>n2;
+    
+    overload o1,o2,sum;
+    o1.get(n1);
+    o2.get(n2);
+    
+    sum=o1+o2;
+    
+    sum.display();
+    
+    return 0;
+    
+}
+
