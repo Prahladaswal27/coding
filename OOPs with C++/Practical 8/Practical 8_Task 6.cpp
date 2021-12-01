fba2c90a6a5b2fde2d78e03c215cf9edaf5ffceb
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2021-12-01 19:12:51 +0530

    Update Practical 8_Task 6.cpp

--- OOPs with C++/Practical 8/Practical 8_Task 6.cpp
+++ OOPs with C++/Practical 8/Practical 8_Task 6.cpp
@@ -1,1 +1,40 @@
 
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+void series(int n);
+
+int main()
+{
+   int n;
+   cout<<"\nEnter last term: ";
+   cin>>n;
+   
+   cout<<"\nSeries is\n";
+   series(n);
+   
+   cout<<endl;
+   return 0;
+   
+}
+
+void series(int n)
+{
+   if (n==1)
+      {
+          cout<<2<<" ";
+          return ;    
+      }
+   else
+      {
+          series(n-1);
+          cout<<(pow(n,n) + n)<<" ";
+        
+      }
+      
+}
+
+
+
