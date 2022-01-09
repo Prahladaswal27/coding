389be98c82c7b24640f862b8af6d219597e00fcd
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-01-09 21:53:07 +0530

    Update Task_1_b.cpp

--- OOPs with C++/Practical 11/Task 1/Task_1_b.cpp
+++ OOPs with C++/Practical 11/Task 1/Task_1_b.cpp
@@ -1,1 +1,29 @@
 
+#include <iostream>
+using namespace std;
+
+class Parent
+{
+    public:
+    void display_Parent()
+    {
+        cout << "Class Parent method called " << endl;
+    }
+};
+class Child : public Parent
+{
+    public:
+    void display_Child()
+    {
+        cout << "Called class Parent method from class Child method" << endl;
+        display_Parent();
+    }
+};
+
+int main()
+{
+    Child obj;
+    obj.display_Parent();
+
+    return 0;
+}
