b5582a398bf8244c04f3c91ff86d461ad1df0b7d
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-01-09 21:54:59 +0530

    Update Task_3_c.cpp

--- OOPs with C++/Practical 11/Task 3/Task_3_c.cpp
+++ OOPs with C++/Practical 11/Task 3/Task_3_c.cpp
@@ -1,1 +1,43 @@
 
+#include <iostream>
+using namespace std;
+
+class B
+{
+
+public:
+    int k;
+    void display_B()
+    {
+        cout << "k in B= " << k << endl;
+    }
+};
+
+class C
+{
+public:
+    int k;
+    void display_c()
+    {
+        cout << "k in C = " << k << endl;
+    }
+};
+
+class D : public B, public C
+{
+public:
+    void display_D()
+    {
+        cout << "k of B and C inherited in D = " << B::k << " " << C::k << endl;
+    }
+};
+
+int main()
+{
+    D obj;
+    obj.B::k = 50;
+    obj.C::k = 100;
+    obj.display_D();
+
+    return 0;
+}
