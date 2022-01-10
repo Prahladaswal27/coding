8600d0a64a1a42ed63e4b9d4553df6bf2b06e0a3
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-01-10 12:42:42 +0530

    Update Task_1_v3.cpp

--- OOPs with C++/Practical 13/Task 1/Version 3/Task_1_v3.cpp
+++ OOPs with C++/Practical 13/Task 1/Version 3/Task_1_v3.cpp
@@ -1,1 +1,61 @@
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Base
+{
+public:
+    int add(int a, int b)
+    {
+        cout<<"\nBase class add func for integer"<<endl;
+        return a + b;
+    }
+    float add(float a, float b)
+    {
+        cout<<"\nBase class add func for float"<<endl;
+        return a + b;
+    }
+    string add(char a, char b)
+    {
+        cout<<"\nBase class add func for string"<<endl;
+        string str;
+        str = str + a;
+        str = str + b;
+        return str;
+    }
+};
+
+class Derived : public Base
+{
+public:
+    using Base::add;
+    int add(int a, int b)
+    {
+        cout<<"\nDerived class add func for interger"<<endl;
+        return a + b + 1;
+    }
+};
+
+int main()
+{
+    Derived obj2;
+    cout << "without using  " << endl
+        <<"..........."<<endl;
+
+    int ans1 = obj2.add(1, 2);
+    cout<<ans1;
+
+    float ans2 = obj2.add(5.5f, 2.15f);
+    cout<<ans2;
+
+    return 0;
+        
+}
+/*
+not visible functions
+without using
+base -int add(int,int)
+      -float add(float ,float)
+      -string add(char,char)
+*/
