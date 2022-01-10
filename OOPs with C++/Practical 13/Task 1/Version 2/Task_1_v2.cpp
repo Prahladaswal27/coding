961371de9cbce833fec123b9498c8a8efdbdf2a7
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-01-10 12:41:28 +0530

    Update Task_1_v2.cpp

--- OOPs with C++/Practical 13/Task 1/Version 2/Task_1_v2.cpp
+++ OOPs with C++/Practical 13/Task 1/Version 2/Task_1_v2.cpp
@@ -1,1 +1,61 @@
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Base
+{
+public:
+    int add(int a, int b)
+    {
+        cout<<"\nBase class add func for integer"<<endl;
+        return a + b;
+    }
+    float add(float a, float b)
+    {
+        cout<<"\nBase class add func for float"<<endl;
+        return a + b;
+    }
+    string add(char a, char b)
+    {
+        cout<<"\nBase class add func for string"<<endl;
+        string str;
+        str = str + a;
+        str = str + b;
+        return str;
+    }
+};
+
+class Child : public Base
+{
+public:
+    using Base::add;
+    int add(int a, int b)
+    {
+        cout<<"\nDerived class add func for interger"<<endl;
+        return a + b + 1;
+    }
+};
+
+int main()
+{
+    Child obj2;
+    cout << "With using " << endl
+         << "............."<<endl;
+
+    int ans1 = obj2.add(1, 2);
+    cout<< ans1 << endl;
+
+    float ans2 = obj2.add(5.5f, 2.15f);
+    cout<< ans2 << endl;
+
+    string ans3 = obj2.add('a', 'b');
+    cout<<ans3;
+
+    return 0;
+}
+/*
+not visible functions
+with using
+Base-add(int ,int)
+*/
