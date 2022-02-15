9f51b079895072bf96a7a87715b4344b2defa11a
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-15 15:33:43 +0530

    Update Practical 3_Task 1.cpp

--- OOPs with C++/Practical 3/Task 1/Practical 3_Task 1.cpp
+++ OOPs with C++/Practical 3/Task 1/Practical 3_Task 1.cpp
@@ -1,1 +1,15 @@
 
+#include <iostream.h>  // Deprecated header file in c++
+using namespace std;
+
+int main()
+{
+    int a, b, multi;
+    cout << "Enter two no. : ";
+    cin >> a >> b;
+
+    multi = a * b;
+
+    cout << "Multiplication of two no. is: " << multi;
+    return 0;
+}
