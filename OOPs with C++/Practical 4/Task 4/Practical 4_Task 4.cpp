0e22eb980a48ba939c4ff0ea4413ea70a1552f0c
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-15 15:39:14 +0530

    Update Practical 4_Task 4.cpp

--- OOPs with C++/Practical 4/Task 4/Practical 4_Task 4.cpp
+++ OOPs with C++/Practical 4/Task 4/Practical 4_Task 4.cpp
@@ -1,1 +1,30 @@
 
+#include <iostream>
+using namespace std;
+
+namespace first
+{
+    int add(int a, int b)
+    {
+        return (a + b);
+    }
+}
+
+namespace second
+{
+    float add(float a, float b)
+    {
+        return (a + b);
+    }
+}
+
+using namespace first;
+
+int main()
+{
+    cout << "\nSum is: " << add(3, 3);  // call first namespace add function
+    cout << "\nADD is: " << add(3.6, 7.8) << endl;  // call first namespace add function
+    cout << "\naddition of two is: " << add(3, 4.7f);  // call first namespace add function
+
+    return 0;
+}
