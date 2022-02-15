01b27ce08b9690b50fea194862cd8d8eb7a98035
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-15 15:32:46 +0530

    Update Practical 2.cpp

--- OOPs with C++/Practical 2/Practical 2.cpp
+++ OOPs with C++/Practical 2/Practical 2.cpp
@@ -1,33 +1,29 @@
-
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-   int n,i,count=0;
-  cout<<"Enter the no.: ";
-  cin>>n;
-  
-  for(i=2;i<=n/2;i++)
-  {
-    if(n%i==0)
+    int n, i, count = 0;
+    cout << "Enter the no.: ";  
+    cin >> n;
+
+    for (i = 2; i <= n / 2; i++)   // loop for checking no. is prime or not
     {
-      count =1;
-      break;
+        if (n % i == 0)
+        {
+            count = 1;
+            break;
+        }
     }
-  }
-  
-  if(n==1)
-    cout<<"\n1 is neither prime nor composite";
-  else
-  {
-    if(count==0)
-        cout<<n<<" is prime no.";
+
+    if (n == 1)
+        cout << "\n1 is neither prime nor composite";
     else
-         cout<<n<<" is not a prime no.";
-  }
-  
-  return 0;
-  
+    {
+        if (count == 0)
+            cout << n << " is prime no.";
+        else
+            cout << n << " is not a prime no.";
+    }
+    return 0;
 }
-    
