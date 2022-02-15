9849ffa6a50a5df35ba130b59cd6a193ac8dd2fd
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-15 15:41:14 +0530

    Update Practical 10_Task 3.cpp

--- OOPs with C++/Practical 10/Task 3/Practical 10_Task 3.cpp
+++ OOPs with C++/Practical 10/Task 3/Practical 10_Task 3.cpp
@@ -1,1 +1,32 @@
 
+#include <iostream>
+using namespace std;
+
+class remainder
+{
+   private:
+    int a, b;
+
+   public:
+    remainder(int a, int b)  //parameterized constructor
+    {
+        this->a = a;  // this pointer use
+        this->b = b;  // this pointer use
+    }
+
+    int res()
+    {
+        return (a % b);
+    }
+};
+
+int main()
+{
+    int a, b;
+    cout << "\nEnter the two no.: ";
+    cin >> a >> b;
+
+    remainder r(a, b); // constructor call
+    cout << "\nRemainder of two no. is: " << r.res() << endl;
+    return 0;
+}
