34e2993043587887b40f24d06fe6f3b6cf3d98e8
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-15 15:37:48 +0530

    Update Practical 4_Task 2.cpp

--- OOPs with C++/Practical 4/Task 2/Practical 4_Task 2.cpp
+++ OOPs with C++/Practical 4/Task 2/Practical 4_Task 2.cpp
@@ -1,1 +1,62 @@
 
+// using first namespace
+// #include <iostream>
+// using namespace std;
+
+// namespace first
+// {
+//     int add(int a, int b)
+//     {
+//         return (a + b);
+//     }
+// }
+
+// namespace second
+// {
+//     float add(float a, float b)
+//     {
+//         return (a + b);
+//     }
+// }
+
+// using namespace first;
+// int main()
+// {
+//     cout << "\nSum is: " << add(3, 3);  // call add function of first namespace
+
+//     cout << "\nADD is: " << add(3.6, 7.8) << endl; // call add function of first namespace
+
+//     //cout<<"\n addition of two is: "<<add(3,4.7f);
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+namespace first
+{
+    int add(int a, int b)
+    {
+        return (a + b);
+    }
+}
+
+namespace second
+{
+    float add(float a, float b)
+    {
+        return (a + b);
+    }
+}
+
+using namespace second;
+int main()
+{
+    cout << "\nSum is: " << add(3, 3); // call add function of second namespace
+
+    cout << "\nADD is: " << add(3.6, 7.8) << endl; /* call add function of second
+                                                namespace*/
+
+    //cout<<"\n addition of two is: "<<add(3,4.7f);
+    return 0;
+}
