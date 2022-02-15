d60003f42da8a5894d5dd303c2d05db4786284ca
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-15 15:32:02 +0530

    Update Practical 1.cpp

--- OOPs with C++/Practical 1/Practical 1.cpp
+++ OOPs with C++/Practical 1/Practical 1.cpp
@@ -1,12 +1,11 @@
-
 #include<iostream>
 using namespace std;
 
 #define max 100
-int main
+
+int main()
 {
-    cout<<"Welcome to C++\n";
-    cout<<"MAX= "<<max;
+    cout<<"Welcome to C++\n";       //  print welcome to c++
+    cout<<"MAX= "<<max;             //  print the max value 
     return 0;
 }
-  
