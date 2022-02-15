5f6a2c47ec80580f6631615603052a5d743d230c
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-15 15:34:58 +0530

    Update Practical 3_Task 3.cpp

--- OOPs with C++/Practical 3/Task 3/Practical 3_Task 3.cpp
+++ OOPs with C++/Practical 3/Task 3/Practical 3_Task 3.cpp
@@ -1,1 +1,15 @@
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char name;
+
+    cout << " Enter the name: ";
+    cin >> name; // take only one character as input
+
+    cout << "\n Name: " << name; // print only one character of the name variable
+
+    return 0;
+}
