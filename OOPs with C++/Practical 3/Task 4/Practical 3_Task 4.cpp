152b8a101f8acb35922f7672414a2bd12977157a
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-15 15:35:23 +0530

    Update Practical 3_Task 4.cpp

--- OOPs with C++/Practical 3/Task 4/Practical 3_Task 4.cpp
+++ OOPs with C++/Practical 3/Task 4/Practical 3_Task 4.cpp
@@ -1,1 +1,18 @@
 
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name;
+
+    cout << " Enter the name: ";
+    getline(cin, name); // read a string or  line from input stream
+
+    cout << "\n Name: " << name; // print whole string from output stream
+
+    return 0;
+}
