443a3bdf0cf78574df1a873decbc6b394e88015a
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 18:54:59 +0530

    Update Practical 6_Task 1.cpp

--- OOPs with C++/Practical 6/Task 1/Practical 6_Task 1.cpp
+++ OOPs with C++/Practical 6/Task 1/Practical 6_Task 1.cpp
@@ -1,1 +1,16 @@
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    string str = "abcd";
+
+    int a = 10;
+    string str2 = str + a; // Invalid show error
+
+    cout << "\n Add string is: " << str2;
+
+    return 0;
+}
