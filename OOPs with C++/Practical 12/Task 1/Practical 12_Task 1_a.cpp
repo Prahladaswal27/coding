07291be851eca6001f7dda8b5b02696f12eb88a7
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 18:08:25 +0530

    Update Practical 12_Task 1_a.cpp

--- OOPs with C++/Practical 12/Task 1/Practical 12_Task 1_a.cpp
+++ OOPs with C++/Practical 12/Task 1/Practical 12_Task 1_a.cpp
@@ -1,30 +1,34 @@
-
 #include <iostream>
 using namespace std;
 class A
 {
- private:
- int a;
- public:
- int b;
- protected:
- int c;
+private:
+    int a;
+
+public:
+    int b;
+
+protected:
+    int c;
 };
+
 class B : protected A
 {
- public:
- void display()
- {
- b = 10;
- c = 15;
- cout << "\nb=" << b << endl
- << "c=" << c<<endl
- <<"\n"<<endl;
- }
+
+public:
+    void display()
+    {
+        b = 10;
+        c = 15;
+        cout << "\nb=" << b << endl
+             << "c=" << c<<endl
+             <<"\n"<<endl;
+    }
 };
 int main()
 {
- B obj;
- obj.display();
- return 0;
+    B obj;
+    obj.display();
+
+    return 0;
 }
