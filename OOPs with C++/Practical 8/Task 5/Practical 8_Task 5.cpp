333ee9ff9bcdfb22e5282320baa125cee9c7691d
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 20:14:31 +0530

    Update Practical 8_Task 5.cpp

--- OOPs with C++/Practical 8/Task 5/Practical 8_Task 5.cpp
+++ OOPs with C++/Practical 8/Task 5/Practical 8_Task 5.cpp
@@ -1,1 +1,31 @@
 
+#include <iostream>
+using namespace std;
+
+int factorial(int); // function declaration
+
+int main()
+{
+    int n, res;
+
+    cout << "\nEnter the number: ";
+    cin >> n;
+
+    res = factorial(n);  // call function
+    cout << "\nFactorial of " << n << " is: " << res << endl;
+    return 0;
+}
+
+int factorial(int num)  // function definition
+{
+    if (num < 0)
+        return -1;
+
+    if (num == 0)
+        return 1;
+
+    else
+    {
+        return (num * factorial(num - 1));  // function call itself
+    }
+}
