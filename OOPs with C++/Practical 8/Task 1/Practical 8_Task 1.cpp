417daffc5e64ef0ad7df16d863576be7195e2460
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 20:00:56 +0530

    Update Practical 8_Task 1.cpp

--- OOPs with C++/Practical 8/Task 1/Practical 8_Task 1.cpp
+++ OOPs with C++/Practical 8/Task 1/Practical 8_Task 1.cpp
@@ -1,1 +1,17 @@
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int *p={10,20,20}; /* Invalid show error 'p' requires one element in initializer  */
+    
+    cout<<*p<<endl;
+    
+    p++;
+    cout<<*p;
+
+
+    return 0;
+    
+}
