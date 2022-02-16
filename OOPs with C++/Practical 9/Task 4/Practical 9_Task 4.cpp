12449f6a8d4c8bfb507507141fcbb8ae6de4c374
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 19:04:40 +0530

    Update Practical 9_Task 4.cpp

--- OOPs with C++/Practical 9/Task 4/Practical 9_Task 4.cpp
+++ OOPs with C++/Practical 9/Task 4/Practical 9_Task 4.cpp
@@ -1,1 +1,53 @@
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class student
+{
+   private:
+    string name;
+    int mark, age;
+
+   public:
+    void get_data(int i)
+    {
+        cout << "\nEnter the " << i + 1 << " student name: ";
+        cin >> name;
+        cout << "Enter the " << i + 1 << " student age: ";
+        cin >> age;
+        cout << "Enter the " << i + 1 << " student marks: ";
+        cin >> mark;
+    }
+
+    friend int print_total(student s[]); // friend function declaration
+};
+
+int print_total(student s[])
+{
+    int sum = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        sum += s[i].mark;   /*friend function access private members 
+                                  using object of class */
+                                                                 
+    }
+
+    cout << "\nSum of all students marks is: " << sum << endl;
+}
+
+int main()
+{
+    int sum = 0;
+    student s[3];
+
+    for (int i = 0; i < 3; i++)
+    {
+        s[i].get_data(i);
+    }
+
+    print_total(s);  //call friend function 
+
+    return 0;
+}
