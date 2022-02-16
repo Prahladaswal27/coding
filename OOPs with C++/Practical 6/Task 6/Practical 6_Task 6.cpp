f7c6fa8c79ee9837d0ceed2bbc2bd18614c38281
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 18:59:42 +0530

    Update Practical 6_Task 6.cpp

--- OOPs with C++/Practical 6/Task 6/Practical 6_Task 6.cpp
+++ OOPs with C++/Practical 6/Task 6/Practical 6_Task 6.cpp
@@ -1,1 +1,19 @@
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str = "GEHU";
+    cout << &str << endl;
+    cout << "\n";
+
+    str[0] = 'J';
+    cout << &str << endl;
+    cout << "\n";
+
+    cout << str << endl;
+    return 0;
+}
