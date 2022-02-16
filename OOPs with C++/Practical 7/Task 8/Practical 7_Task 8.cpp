f75a883982d4e908e2a819eb8d2badf3202448eb
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 19:59:00 +0530

    Update Practical 7_Task 8.cpp

--- OOPs with C++/Practical 7/Task 8/Practical 7_Task 8.cpp
+++ OOPs with C++/Practical 7/Task 8/Practical 7_Task 8.cpp
@@ -1,1 +1,30 @@
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char A[4][4];
+    int i, j;
+
+    for (i = 0; i < 4; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            A[i][j] = '*';
+        }
+    }
+
+    cout << endl;
+
+    for (i = 3; i >= 0; i--)
+    {
+        for (j = 0; j <= i; j++)
+        {
+            cout << A[i][j];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
