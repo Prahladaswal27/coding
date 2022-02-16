d3b47adab5d1a361cf69e09805429f52b5eef36f
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 19:02:09 +0530

    Update Practical 9_Task 1_b.cpp

--- OOPs with C++/Practical 9/Task 1/Practical 9_Task 1_b.cpp
+++ OOPs with C++/Practical 9/Task 1/Practical 9_Task 1_b.cpp
@@ -1,1 +1,63 @@
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+class student
+{
+private:
+    string name;
+    int age, year, marks;
+    char sec;
+
+public:
+    void get_data(int i)
+    {
+        cout << "\nEnter the " << i + 1 << " student name: ";
+        cin >> name;
+        cout << "Enter the " << i + 1 << " student section: ";
+        cin >> sec;
+        cout << "Enter the " << i + 1 << " student age: ";
+        cin >> age;
+        cout << "Enter the " << i + 1 << " student marks: ";
+        cin >> marks;
+        cout << "Enter the current year: ";
+        cin >> year;
+    }
+
+    void print_data(int i)
+    {
+        cout << "\n   Student details     " << endl;
+        cout << "\nStudent " << i + 1 << " name: " << name;
+        cout << "\nStudent " << i + 1 << " section: " << sec;
+        cout << "\nStudent " << i + 1 << " age: " << age;
+        cout << "\nStudent " << i + 1 << " marks: " << marks;
+        cout << "\nCurrent year: " << year << endl;
+    }
+    int total()
+    {
+        int sum = 0;
+        sum += marks;
+        return sum;
+    }
+};
+
+int main()
+{
+    student s[3]; // array of objects
+    int sum = 0;
+
+    for (int i = 0; i < 3; i++) // loop for input data of student
+    {
+        s[i].get_data(i);
+    }
+    for (int i = 0; i < 3; i++) // loop for print data of student
+    {
+        s[i].print_data(i);
+        sum += s[i].total();
+    }
+
+    cout << "\nTotal of marks is: " << sum << endl; // print sum of all students marks
+
+    return 0;
+}
