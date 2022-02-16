992bc4cc5866a87831b3bcd41cdc492aafe7c4b7
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 20:09:12 +0530

    Update Practical 8_Task 3_V2.cpp

--- OOPs with C++/Practical 8/Task 3/V2/Practical 8_Task 3_V2.cpp
+++ OOPs with C++/Practical 8/Task 3/V2/Practical 8_Task 3_V2.cpp
@@ -1,1 +1,23 @@
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int *p;
+    int **q;
+
+    p = &a;  //pointer p stores address of variable a
+    q = &p; //pointer q stores  address of pointer p
+
+    cout << *p << endl;  //print value of a
+    cout << **q << endl; //print value of a
+
+    **q = **q / 5;  // divide value of a by 5
+
+    cout << "Value of **q is: " << **q << endl;  // print new value of a
+    cout << "Value of a is: " << a << endl;    // print new value of a
+
+    return 0;
+}
