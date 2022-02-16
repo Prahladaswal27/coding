eb06b9935671f9ec10cfee778f20c9e15ccbe69e
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 19:48:41 +0530

    Update Practical 7_Task 4.cpp

--- OOPs with C++/Practical 7/Task 4/Practical 7_Task 4.cpp
+++ OOPs with C++/Practical 7/Task 4/Practical 7_Task 4.cpp
@@ -1,1 +1,32 @@
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int i, j;
+    cout << "\nMatrix A" << endl;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+            cout << a[i][j] << " ";
+
+        cout << endl;
+    }
+
+    cout << "\nRestore array A" << endl;
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            a[i][j] = a[i][j] - (2 * i + j);
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "\nElement in a[1][1] after restore is: " << a[1][1] << endl;
+    return 0;
+}
