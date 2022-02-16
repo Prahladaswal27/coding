89f946d3803d931e091dbba758cbd477cb0f0be5
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 20:15:29 +0530

    Update Practical 8_Task 6.cpp

--- OOPs with C++/Practical 8/Task 6/Practical 8_Task 6.cpp
+++ OOPs with C++/Practical 8/Task 6/Practical 8_Task 6.cpp
@@ -1,1 +1,32 @@
 
+#include <iostream>
+#include <math.h>
+
+using namespace std; // function declaration
+void series(int n);
+
+int main()
+{
+    int n;
+    cout << "\nEnter last term: ";
+    cin >> n;
+
+    cout << "\nSeries is\n";
+    series(n);  // call function
+    cout << endl;
+    return 0;
+}
+
+void series(int n) // function definition
+{
+    if (n == 1)
+    {
+        cout << 2 << " ";
+        return;
+    }
+    else
+    {
+        series(n - 1); // // recursive function
+        cout << (pow(n, n) + n) << " ";  
+    }
+}
