7c226ddb4202217f3eb6e0b7cb923b2b988dcfd2
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 18:57:25 +0530

    Update Practical 6_Task 3.cpp

--- OOPs with C++/Practical 6/Task 3/Practical 6_Task 3.cpp
+++ OOPs with C++/Practical 6/Task 3/Practical 6_Task 3.cpp
@@ -1,1 +1,31 @@
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string str;
+
+    cout << "Enter the string: ";
+    getline(cin, str);
+
+    cout << "\n\nBy reverse_iterator" << endl;
+
+    cout << "Reverse of string is: ";
+
+    for (string::reverse_iterator ptr = str.rbegin(); ptr != str.rend(); ptr++)
+    {
+        cout << *ptr;
+    }
+
+    cout << "\n\nBy reverse method" << endl;
+
+    reverse(str.begin(), str.end());
+
+    cout << "Reverse of string is: " << str << endl;
+
+    return 0;
+}
