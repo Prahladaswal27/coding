695eef83869ed3b0220692c9785bf4f6a5cc15fc
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 17:49:15 +0530

    Update Practical 12_Task 2_a.cpp

--- OOPs with C++/Practical 12/Task 2/Practical 12_Task 2_a.cpp
+++ OOPs with C++/Practical 12/Task 2/Practical 12_Task 2_a.cpp
@@ -1,1 +1,68 @@
 
+#include <iostream>
+
+using namespace std;
+
+class A
+
+{
+
+ public:
+
+ A()
+
+ {
+
+ cout << "\nExecution of Class A Constructor" << endl;
+
+ }
+
+};
+
+class B : public A
+
+{
+
+ public:
+
+ B()
+
+ {
+
+ cout << "\nExecution of Class B Constructor" << endl;
+
+ }
+
+};
+
+class C : public B
+
+{
+
+public:
+
+ C()
+
+ {
+
+ cout << "\nExecution of Class C Constructor" 
+
+<<"\n"<<endl;
+
+ }
+
+};
+
+int main()
+
+{
+
+ cout << "\nCalled Constructor of C" << endl
+
+ << "............." << endl;
+
+ C obj;
+
+ return 0;
+
+}
