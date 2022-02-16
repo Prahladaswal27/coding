e33db0f9ff649ff0be33be1acd3e960d18b470f4
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 19:05:33 +0530

    Update Practical 9_Task 5.cpp

--- OOPs with C++/Practical 9/Task 5/Practical 9_Task 5.cpp
+++ OOPs with C++/Practical 9/Task 5/Practical 9_Task 5.cpp
@@ -1,1 +1,25 @@
 
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+struct student
+{
+    private:
+
+};
+int main()
+{
+
+}
+
+/* Difference Between structure and class 
+
+   1. Class- Members of a class are private by default
+      Structure  of a structure are public by default.
+   2. Memory allocation happens on the heap. In structure emory allocation happens on a stack.
+   3. Inheritance is possible in class.	Inheritance is not possible in structures.
+   4. Member variables of a class can be initiated directly.	Member variables of a structure cannot be initiated directly.
+
+*/
