2b834d1a75728ff38f1b47eaa600e06c2486314b
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 20:05:01 +0530

    Update Practical 8_Task 2_V2.cpp

--- OOPs with C++/Practical 8/Task 2/Version 2/Practical 8_Task 2_V2.cpp
+++ OOPs with C++/Practical 8/Task 2/Version 2/Practical 8_Task 2_V2.cpp
@@ -1,1 +1,14 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int arr[]={10,20,30};
+    cout<<*arr<<endl;  // print value at index 0
+    
+    cout<<arr<<endl; // print base address(address of index 0)
+    
+    cout<<*(arr+1)<<endl; // print value at index 1
+    return 0;
+    
+}
