74f8393757f963753f7aabc4f052c3fe7f82df17
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 19:06:53 +0530

    Update Practical 9_Task 6.cpp

--- OOPs with C++/Practical 9/Task 6/Practical 9_Task 6.cpp
+++ OOPs with C++/Practical 9/Task 6/Practical 9_Task 6.cpp
@@ -1,1 +1,24 @@
 
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    cout << "\nUSE OF endl" << endl;
+   cout << "GEHU" << endl<< "DEHRADUN" << endl<<"UTTARAKHAND";
+      //Insert newline characters and  flushes the stream 
+
+
+    cout << "\n\nUSE OF setw()\n";
+    cout << setw(8) << "DEHRADUN" << endl;  
+    // setw sets the width to beused on output operations
+
+    cout << "\nUSE OF setfill()\n";
+    cout << setw(12) << setfill('*') << "DEHRADUN" << endl;
+    /* setfill is used for specify a different character to fill the unused
+       filled width of the value */
+
+    return 0;
+}
