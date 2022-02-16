5bb0452d3d1752138177f05b21c40a0f9be355b7
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 19:00:52 +0530

    Update Practical 9_Task 1_a.cpp

--- OOPs with C++/Practical 9/Task 1/Practical 9_Task 1_a.cpp
+++ OOPs with C++/Practical 9/Task 1/Practical 9_Task 1_a.cpp
@@ -1,1 +1,39 @@
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class student
+{
+    public:
+      string add;
+      int roll_no;
+      long int phn_no;
+
+    void print_data(string s) // function to print details of student
+    {
+        cout << "\n   Student details     " << endl;
+
+        cout << "\nStudent name: " << s;
+        cout << "\nStudent roll no: " << roll_no;
+        cout << "\nStudent phone number: " << phn_no;
+        cout << "\nStudent address: " << add << endl;
+    }
+};
+
+int main()
+{
+    student john, sam; // student class object
+
+    john.roll_no = 2018554;
+    john.phn_no = 9123846345;
+    john.add = "15 george street, california,USA";
+
+    sam.roll_no = 2018559;
+    sam.phn_no = 9875219053;
+    sam.add = "17 peter street, california,USA";
+
+    john.print_data("John"); // call print data function of john object
+    sam.print_data("Sam");   // call print data function of sam object
+}
