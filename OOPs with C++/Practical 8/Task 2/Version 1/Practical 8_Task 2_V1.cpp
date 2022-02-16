311fc1f7c7e5c63e0fa7d97db20985d876d16a70
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 20:03:17 +0530

    Update Practical 8_Task 2_V1.cpp

--- OOPs with C++/Practical 8/Task 2/Version 1/Practical 8_Task 2_V1.cpp
+++ OOPs with C++/Practical 8/Task 2/Version 1/Practical 8_Task 2_V1.cpp
@@ -1,1 +1,17 @@
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[]={10,20,30};
+    cout<<*arr<<endl;
+    
+    cout<<arr;
+    arr++;   // Invalid 
+    
+    cout<<*arr;
+    
+    return 0;
+    
+} 
