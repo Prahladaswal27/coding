58de743b3c663d370d8d0d8d35551da961d91494
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 18:17:28 +0530

    Update and rename Task_1_v3.cpp to Practical 13_Task 1_v3.cpp

--- OOPs with C++/Practical 13/Task 1/Version 3/Practical 13_Task 1_v3.cpp
+++ OOPs with C++/Practical 13/Task 1/Version 3/Practical 13_Task 1_v3.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <string>
 using namespace std;
@@ -40,14 +39,14 @@ public:
 int main()
 {
     Derived obj2;
-    cout << "without using  " << endl
+    cout << "\nWithout using  " << endl
         <<"..........."<<endl;
 
-    int ans1 = obj2.add(1, 2);
+    int ans1 = obj2.add(15, 25);
     cout<<ans1;
 
-    float ans2 = obj2.add(5.5f, 2.15f);
-    cout<<ans2;
+    float ans2 = obj2.add(8.5f, 3.15f);
+    cout<<ans2<<"\n"<<endl;
 
     return 0;
         
