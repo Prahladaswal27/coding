f44ef33e71d5ab95d550e9f7e92a5af19cc32f05
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 20:16:28 +0530

    Update Practical 8_Task 7_V2.cpp

--- OOPs with C++/Practical 8/Task 7/V2/Practical 8_Task 7_V2.cpp
+++ OOPs with C++/Practical 8/Task 7/V2/Practical 8_Task 7_V2.cpp
@@ -1,1 +1,31 @@
 
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b); // function declaration
+int main()
+{
+    int a = 10, b = 20;
+
+    cout << "\nBefore swap function" << endl;
+    cout << "a= " << a << " b= " << b << endl;
+
+    swap(a, b); // function call
+
+    cout << "\nAfter swap function" << endl;
+    cout << "a= " << a << " b= " << b << endl;
+
+    return 0;
+}
+
+void swap(int &a, int &b) // function definition
+{
+    int temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "\nValues is swap function" << endl;
+    cout << "a= " << a << " b= " << b << endl;
+}
