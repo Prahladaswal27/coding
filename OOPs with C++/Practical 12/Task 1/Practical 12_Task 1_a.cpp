9a4222f813c2e19e8119ba0c33315384312c7920
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 15:26:29 +0530

    Update Practical 12_Task 1_a.cpp

--- OOPs with C++/Practical 12/Task 1/Practical 12_Task 1_a.cpp
+++ OOPs with C++/Practical 12/Task 1/Practical 12_Task 1_a.cpp
@@ -1,1 +1,30 @@
 
+#include <iostream>
+using namespace std;
+class A
+{
+ private:
+ int a;
+ public:
+ int b;
+ protected:
+ int c;
+};
+class B : protected A
+{
+ public:
+ void display()
+ {
+ b = 10;
+ c = 15;
+ cout << "\nb=" << b << endl
+ << "c=" << c<<endl
+ <<"\n"<<endl;
+ }
+};
+int main()
+{
+ B obj;
+ obj.display();
+ return 0;
+}
