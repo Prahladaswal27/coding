2804aa81185413c8dcef485e5167a3495710bf84
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-02-16 18:11:37 +0530

    Update Practical 12_Task 2_c.cpp

--- OOPs with C++/Practical 12/Task 2/Practical 12_Task 2_c.cpp
+++ OOPs with C++/Practical 12/Task 2/Practical 12_Task 2_c.cpp
@@ -1,1 +1,44 @@
 
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+    int a;
+    A(int count)
+    {
+        a = count;
+        cout << "\na = " << a << endl;
+    }
+};
+
+class B : public A
+{
+
+public:
+    int b;
+    B(int count):A(count)
+    {
+        b = count;
+        cout << "\nb = " << b << endl;
+    }
+};
+
+class C : public B
+{
+    int c;
+
+public:
+    C(int count):B(count)
+    {
+        c = count;
+        cout << "\nc = " << c << endl;
+    }
+};
+
+int main()
+{
+    C obj(40);
+
+    return 0;
+}
