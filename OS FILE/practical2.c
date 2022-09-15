63defe6e50e0f49caad0faf6a95e70f83e09152f
Prahladaswal27 <91825078+Prahladaswal27@users.noreply.github.com>
2022-09-15 22:37:46 +0530

    Update practical2.c

--- OS FILE/practical2.c
+++ OS FILE/practical2.c
@@ -1,1 +1,45 @@
 
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    printf("Name – Prahlad Singh Aswal \nSection - A ");
+    printf("\nStudent ID - 20011794\n\n");
+    
+    int even_sum = 0, odd_sum = 0; 
+    int n;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    int arr[n];
+
+    printf("Enter numbers:\n");
+    
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int pid = fork();
+    if (pid == 0)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] % 2 != 0)
+                odd_sum += arr[i];
+        }
+        printf("Sum of Odd Numbers: %d\n", odd_sum);
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] % 2 == 0)
+                even_sum += arr[i];
+        }
+        printf("Sum of Even Numbers: %d\n", even_sum);
+    }
+
+    return 0;
+}
